97094fece5c2996e3b2843a5c8e77b29045c9bd1
lukeIam <2lukeiam@gmail.com>
2025-04-30 21:19:37 +0100

    extend ignore_chapters

--- beetsplug/audible.py
+++ beetsplug/audible.py
@@ -213,7 +213,7 @@ class Audible(BeetsPlugin):
                 # I was noticing inaccurate sorting if using str to convert paths to strings
                 naturally_sorted_items = os_sorted(items, key=lambda i: util.bytestring_path(i.path))
                 a.tracks = [
-                    TrackInfo(**common_track_attributes, title=item.title, length=item.length, index=i + 1)
+                    TrackInfo(**common_track_attributes, title=item.title if not self.config["ignore_chapters"] else f"{item.album_sort} - {(i + 1):03}", length=item.length, index=i + 1)
                     for i, item in enumerate(naturally_sorted_items)
                 ]
         return albums
@@ -272,7 +272,7 @@ class Audible(BeetsPlugin):
                 artist=artists,
                 index=i + 1,
                 length=item.length,
-                title=item.title,
+                title=item.title if not self.config["ignore_chapters"] else f"{album_sort} - {(i + 1):03}",
                 medium=1,
             )
             for i, item in enumerate(naturally_sorted_items)
